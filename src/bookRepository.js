52dbf75fdc4213d1e3e0fdab09f93547f7c8a176
kwasniew <kwasniewski.mateusz@gmail.com>
2019-10-16 16:09:44 +0200

    book service

--- src/bookRepository.js
+++ src/bookRepository.js
@@ -10,11 +10,11 @@ let booksPromise = MongoClient
     });
 
 module.exports = {
-    async createOrUpdate({title, authors, isbn, description}) {
+    async createOrUpdate({title, slug, authors, isbn, description}) {
         const books = await booksPromise;
         await books.updateOne(
             {isbn: isbn},
-            {$set: {title, authors, isbn, description}},
+            {$set: {title, slug, authors, isbn, description}},
             {upsert: true}
         );
     },
--- test/component/appTest.js
+++ test/component/appTest.js
@@ -3,8 +3,10 @@ const assert = require('assert');
 const app = require('../../src/app');
 
 describe('Book inventory', function () {
-    it('allows to stock up the items', function (done) {
-        httpClient(app)
+    it('allows to stock up the items', async function () {
+        const request = httpClient(app);
+        // POST/REDIRECT
+        const createResult = await request
             .post('/book')
             .send({
                 title: "JavaScript in Action",
@@ -13,11 +15,16 @@ describe('Book inventory', function () {
                 description: "The ultimate JS book!"
             })
             .set('Content-Type', 'application/json')
-            .expect(200, {
-                title: "JavaScript in Action",
-                authors: ["James Smith", "Kate Donovan"],
-                isbn: "0123456789",
-                description: "The ultimate JS book!"
-            }, done);
+            .expect(302);
+        // GET
+        const readResult = await request.get(createResult.header.location).expect(200);
+
+        assert.deepStrictEqual(readResult.body, {
+            title: "JavaScript in Action",
+            slug: "javascript-in-action",
+            authors: ["James Smith", "Kate Donovan"],
+            isbn: "0123456789",
+            description: "The ultimate JS book!"
+        });
     })
 });
