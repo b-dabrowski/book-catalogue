2d9b8072e31d87a1b4c26815953d6806cf0cbd42
kwasniew <kwasniewski.mateusz@gmail.com>
2019-10-16 14:23:06 +0200

    promises

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+
+
+// const url = 'mongodb://db:27017/booksapi'; // when using docker-compose for full development
+const url = 'mongodb://localhost:27017/booksapi1';
+
+let booksPromise = MongoClient
+    .connect(url)
+    .then(function (client) {
+        return client.db().collection("books");
+    });
+
 // duck typing
 function log(req, res, next) {
-    console.log("new request at " + new Date());
+    // console.log("new request at " + new Date());
     next();
 }
 
 function auth(req, res, next) {
-    console.log("auth");
+    // console.log("auth");
     next();
 }
 
@@ -22,12 +34,34 @@ app.get("/", function (req, res) {
 app.get("/hello", function (req, res) {
     res.send("Hello World!");
 });
-app.post("/book", function(req, res) {
-    // destructuring from ES6
+
+app.post("/book", function (req, res) {
+
     const {title, authors, isbn, description} = req.body;
+    booksPromise.then(function (books) {
+        books.updateOne(
+            {isbn: isbn},
+            {$set: {title, authors, isbn, description}},
+            {upsert: true}
+        );
+    });
+
     res.json({title, authors, isbn, description});
 });
-app.use(function(req, res, next) {
+
+app.get("/book/:isbn", function (req, res, next) {
+    const isbn = req.params.isbn;
+    booksPromise
+        .then(function (books) {
+            return books.findOne({isbn}, {projection: {_id: 0}});
+        })
+        .then(function (book) {
+            res.json(book);
+        })
+        .catch(next);
+
+});
+app.use(function (req, res, next) {
     const err = new Error("not found");
     err.status = 404;
     next(err);
