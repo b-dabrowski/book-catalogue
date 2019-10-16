41684cf4bfbf9fa5aa47a569f6c874fa53c2d4a5
kwasniew <kwasniewski.mateusz@gmail.com>
2019-10-16 15:23:29 +0200

    book controller

--- src/bookRoutes.js
+++ src/bookRoutes.js
@@ -1,40 +1,7 @@
 const router = require("express").Router();
-const MongoClient = require('mongodb').MongoClient;
+const {createOrUpdate, details} = require("./bookController");
 
-// const url = 'mongodb://db:27017/booksapi'; // when using docker-compose for full development
-const url = 'mongodb://localhost:27017/booksapi1';
-
-let booksPromise = MongoClient
-    .connect(url, {bufferMaxEntries: 0, useNewUrlParser: true})
-    .then(function (client) {
-        return client.db().collection("books");
-    });
-
-router.post("/book", async function (req, res, next) {
-    try {
-        const {title, authors, isbn, description} = req.body;
-
-        const books = await booksPromise;
-        await books.updateOne(
-            {isbn: isbn},
-            {$set: {title, authors, isbn, description}},
-            {upsert: true}
-        );
-        res.json({title, authors, isbn, description});
-    } catch (e) {
-        next(e);
-    }
-});
-
-router.get("/book/:isbn", async function (req, res, next) {
-    try {
-        const isbn = req.params.isbn;
-        const books = await booksPromise;
-        const book = await books.findOne({isbn}, {projection: {_id: 0}});
-        res.json(book);
-    } catch(e) {
-        next(e);
-    }
-});
+router.post("/book", createOrUpdate);
+router.get("/book/:isbn", details);
 
 module.exports = router;
